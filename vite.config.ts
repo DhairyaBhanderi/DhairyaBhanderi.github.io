f1b5fa12f3adbbbebd499b51ff2aa81b2c135e73
DhairyaBhanderi <bhanderidhairyaj@gmail.com>
2025-12-29 23:03:14 -0500

    Configure base path in Vite config
    Set base path for user-site root.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ important for user-site root
   server: {
     host: "::",
     port: 8080,
